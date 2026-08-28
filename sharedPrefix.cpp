d726a3a235c3be5a8feace74d831efd63343934d
aemalyutin <aemalyutin@sberbank.ru>
2026-08-29 01:04:27 +0300

    Начал решать задачу на общий префикс

---
+++ sharedPrefix.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+/*
+Напишите функцию для вычисления наибольшего общего префикса строк, переданных в векторе words:
+std::string CommonPrefix(const std::vector<std::string>& words);
+Например, для пустого вектора функция должна вернуть пустую строку, а для вектора из строк "apple", "apricot" и "application" — строку "ap".
+Примечание
+В решении не должно быть функции main: она будет в нашей тестирующей программе. Подключите необходимые библиотеки и напишите код функции CommonPrefix.
+*/
+std::string CommonPrefix(const std::vector<std::string> &words)
+{
+
+  return;
+}
+
+int main()
+{
+  return 0;
+}
