7e6912393155a7c7b46f758d6bebb1e86e74d51c
aemalyutin <aemalyutin@gmail.com>
2026-08-31 11:33:14 +0300

    Решил задачу на общий префикс

--- sharedPrefix.cpp
+++ sharedPrefix.cpp
@@ -1,19 +1,60 @@
 #include <iostream>
 #include <vector>
+#include <string>
 /*
 Напишите функцию для вычисления наибольшего общего префикса строк, переданных в векторе words:
 std::string CommonPrefix(const std::vector<std::string>& words);
-Например, для пустого вектора функция должна вернуть пустую строку, а для вектора из строк "apple", "apricot" и "application" — строку "ap".
+Например, для пустого вектора функция должна вернуть пустую строку, а для вектора из строк "apple",
+ "apricot" и "application" — строку "ap".
 Примечание
-В решении не должно быть функции main: она будет в нашей тестирующей программе. Подключите необходимые библиотеки и напишите код функции CommonPrefix.
+В решении не должно быть функции main: она будет в нашей тестирующей программе. Подключите
+необходимые библиотеки и напишите код функции CommonPrefix.
 */
 std::string CommonPrefix(const std::vector<std::string> &words)
 {
-
-  return;
+  if (words.empty() || words[0].empty())
+  {
+    return "";
+  }
+  std::string strPref = words[0].substr(0, 1);
+  bool compareOk = true;
+  size_t pos = 1;
+  while (compareOk)
+  {
+    for (size_t j = 1; j != words.size(); ++j)
+    {
+      if (words[j].find(strPref) != 0)
+      {
+        compareOk = false;
+        break;
+      }
+    }
+    if (compareOk)
+    { 
+      if (pos < words[0].size()) {
+        strPref += words[0][pos];
+        ++pos;
+      } else {
+        break;
+      }
+    }
+  }
+  if (!compareOk) {
+    return strPref.substr(0, strPref.size() - 1);
+  } 
+  return strPref;
 }
 
 int main()
 {
+  std::vector<std::string> words;
+  std::string result;
+  std::string word;
+  while (std::getline(std::cin, word))
+  {
+    words.push_back(word);
+  }
+  result = CommonPrefix(words);
+  std::cout << result << std::endl;
   return 0;
 }
