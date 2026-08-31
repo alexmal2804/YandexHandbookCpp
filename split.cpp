9d017b8619f7dd6f1be35647e5f6199903b2b748
aemalyutin <aemalyutin@sberbank.ru>
2026-08-31 18:10:59 +0300

    Исправил ошибки в задаче \ на сплит

--- split.cpp
+++ split.cpp
@@ -16,19 +16,20 @@ std::vector<std::string> Split(const std::string &str, char delimiter)
   std::vector<std::string> resVector;
   if (str.empty())
   {
-    return {};
+    return {""};
   }
   size_t posBeginWord = 0;
   size_t posEndWord = str.find(delimiter, posBeginWord);
+  if (posEndWord == std::string::npos)
+  {
+    posEndWord = str.size();
+  }
   resVector.push_back(str.substr(posBeginWord, posEndWord - posBeginWord));
   while (posEndWord != str.size())
   {
     posBeginWord = posEndWord + 1;
     posEndWord = str.find(delimiter, posBeginWord) == std::string::npos ? str.size() : str.find(delimiter, posBeginWord);
-    if (posEndWord - posBeginWord > 0)
-    {
-      resVector.push_back(str.substr(posBeginWord, posEndWord - posBeginWord));
-    }
+    resVector.push_back(str.substr(posBeginWord, posEndWord - posBeginWord));
   }
   return resVector;
 }
