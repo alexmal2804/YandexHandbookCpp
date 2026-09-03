3bfffd6fc30eec79794f12f50983c12da14be66c
aemalyutin <aemalyutin@gmail.com>
2026-09-03 09:49:51 +0300

    Начал решать задачу на двухсторнюю очередь

---
+++ testPaper.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <deque>
+#include <string>
+/*
+
+*/
+enum class Plase {
+  top = 0,
+  bottom = 1
+};
+struct Test {
+  std::string Family;
+  Plase Plase;
+};
+
+std::deque<Test> StackOfPapers (size_t n){
+  for (size_t i = 0; i != n; ++i){
+      
+  }
+}
+
+int main()
+{
+  return 0;
+}
