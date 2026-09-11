19b51495843b1e939ecfb72104c667e51c5d31d4
aemalyutin <aemalyutin@sberbank.ru>
2026-09-11 21:33:14 +0300

    Решил  задачу на дублированный вектор

--- doublicateVector.cpp
+++ doublicateVector.cpp
@@ -1,24 +1,18 @@
 #include <iostream>
-
 #include <vector>
 
 template <typename T>
 void Duplicate(std::vector<T> &v)
 {
-  for (auto it = v.begin(); it != v.end() - 1; ++it)
-  {
-    if (it != v.end())
-    {
-      v.push_back(*it);
-    }
-  }
+  std::vector<T> tmp = v;
+  std::copy(tmp.begin(), tmp.end(), back_inserter(v));
 }
 
 int main()
 {
   std::vector<int> v1 = {1, 2, 3};
   Duplicate(v1);
-  for (const auto &v : v1)
+  for (const auto v : v1)
   {
     std::cout << v << " ";
   }
