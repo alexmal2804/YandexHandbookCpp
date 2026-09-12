318efd78c39c75454ba691a44ea45e47e5bf575e
aemalyutin <aemalyutin@sberbank.ru>
2026-09-12 17:56:56 +0300

    Начал делать собственную реализацию алгоритма Unique

--- unique.cpp
+++ unique.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 template <typename Iter>
 Iter Unique(Iter first, Iter last)
 {
@@ -6,5 +8,16 @@ Iter Unique(Iter first, Iter last)
 }
 int main()
 {
+  std::vector<int> v = {5, 5, 3, 2, 2, 5, 9, 2};
+  auto it = Unique(v.begin(), v.end());
+  for (size_t i = 0; i != v.size(); ++i)
+  {
+    if (i != 0)
+    {
+      std::cout << " ";
+    }
+    std::cout << v[i];
+  }
+  std::cout << "\n";
   return 0;
 }
