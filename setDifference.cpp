468aa515c2d0fd4c1a118b4f9ed07123c8f4828e
aemalyutin <aemalyutin@sberbank.ru>
2026-09-12 22:40:02 +0300

    Продолжил  делать собственную реализацию алгоритма set_difference 2

--- setDifference.cpp
+++ setDifference.cpp
@@ -7,17 +7,38 @@ OutIter SetDifference(InIter1 first1, InIter1 last1,
                       InIter2 first2, InIter2 last2,
                       OutIter out)
 {
-
+  while (first1 < last1)
+    if (*first1 < *first2)
+    {
+      *out = *first1;
+      ++out;
+      ++first1;
+    }
+    else if (*first1 > *first2)
+    {
+      ++first2;
+    }
+    else if (*first1 == *first2)
+    {
+      ++first1;
+      ++first2;
+    }
+    else
+    {
+      *out = *first1;
+      ++out;
+      ++first1;
+    }
   return out;
 };
 int main()
 {
-  std::vector<int> v1 = {5, 5, 3, 2, 2, 5, 9, 2, 5, 3, 3, 2};
-  std::vector<int> v2 = {5, 2, 3, 4, 2, 5, 7, 2, 5, 3, 2};
+  std::vector<int> v1 = {1, 3, 5, 5, 7};
+  std::vector<int> v2 = {1, 1, 2, 3};
   std::list<int> v;
   std::sort(v1.begin(), v1.end());
   std::sort(v2.begin(), v2.end());
-  auto it = SetDifference(v1.begin(), v1.end(), v2.begin(), v2.end(), v.begin());
+  auto it = SetDifference(v1.begin(), v1.end(), v2.begin(), v2.end(), std::back_inserter(v));
   const char *separator = "";
   for (const auto &element : v)
   {
