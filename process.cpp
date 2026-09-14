8a8ab07e03ceec57412bce8c481af88b0476a2a5
aemalyutin <aemalyutin@gmail.com>
2026-09-14 15:40:19 +0300

    Начал решать задачу Шаблонная функция Process

---
+++ process.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+/*
+Андрею надо написать шаблонную функцию Process, которая обрабатывает вектор
+с числами некоторого типа T. Его функция должна вызвать другую функцию
+ PrintResults, чтобы напечатать с определенным форматированием положительные
+  числа из вектора. Функция PrintResults принимает на вход пару итераторов,
+  как и многие алгоритмы стандартной библиотеки. Поэтому Андрей решил сначала
+   скопировать нужные элементы исходного вектора в другой массив, чтобы передать
+    его начало и конец в эту функцию. Вот код Андрея:
+
+Этот код почему-то не работает. Найдите ошибку и сдайте исправленное решение.
+*/
+ template <typename T>
+  void Process(const std::vector<T> &data)
+  {
+    std::vector<T> filtered;
+
+    auto filteredLast = std::copy_if(
+        data.begin(),
+        data.end(),
+        filtered.begin(),
+        [](const T &x)
+        { return x > 0; });
+
+    PrintResults(filtered.begin(), filteredLast);
+  }
+int main()
+{
+ 
+  return 0;
+}
