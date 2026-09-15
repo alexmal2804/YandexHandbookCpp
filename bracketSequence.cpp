5a0eadefc81927bbca27eb979c678a71950bb1cc
aemalyutin <aemalyutin@gmail.com>
2026-09-15 14:47:26 +0300

    Решаю  задачу на скобочную последовательность

--- bracketSequence.cpp
+++ bracketSequence.cpp
@@ -2,5 +2,15 @@
 
 int main()
 {
+  std::string bracketSeq;
+  std::cin >> bracketSeq;
+  if (bracketSeq[0] == '}' || bracketSeq[0] == ')' || bracketSeq[0] == ']'){
+    std::cout << "NO" << "\n";
+    return 0;
+  }
+  bool isBracketOrder = false;
+  for (size_t i = 0; i != bracketSeq.size(); ++i){
+    
+  }
   return 0;
 }
