9fe6ea8eabe8fd23c3aad0c789554af5dc87d187
Alex <alexmal2804@gmail.com>
2026-09-25 21:16:16 +0000

    Решил задачу на функцию NextToken

--- nextToken.cpp
+++ nextToken.cpp
@@ -1,30 +1,43 @@
+#include <cstddef>
 #include <iostream>
 #include <string_view>
 /*
-Вам надо написать функцию NextToken для выделения очередного токена в строке. 
-Токеном считается последовательность символов до указанного символа-разделителя (или до конца строки).
-Сдайте только код функции NextToken и подключите необходимые библиотеки. 
-Ваша функция будет скомпилирована с нашей функцией main. 
-Гарантируется, что входная строка не заканчивается на разделитель. 
-Догадайтесь сами, какие аргументы должна принимать функция NextToken. 
+Вам надо написать функцию NextToken для выделения очередного токена в строке.
+Токеном считается последовательность символов до указанного символа-разделителя
+(или до конца строки). Сдайте только код функции NextToken и подключите
+необходимые библиотеки. Ваша функция будет скомпилирована с нашей функцией main.
+Гарантируется, что входная строка не заканчивается на разделитель.
+Догадайтесь сами, какие аргументы должна принимать функция NextToken.
 Эта функция может менять первый аргумент (sv).
 */
-int NextToken(std::string_view &sv, const char delimiter, std::string_view &token) {
-  return 0;
+bool NextToken(std::string_view &sv, const char delimiter,
+               std::string_view &token) {
+  const size_t ePos = sv.find(delimiter);
+  if (sv.size() == 0) {
+    return false;
+  }
+  if (ePos == std::string_view::npos) {
+    token = sv;
+    sv = "";
+    return true;
+  }
+  const size_t bPos = 0;
+  token = sv.substr(bPos, ePos);
+  sv = sv.substr(ePos + 1);
+  return true;
 }
 
-int main()
-{
-    std::string_view sv = "Hello world and good bye";
+int main() {
+  std::string_view sv = "Hello world and good bye";
 
-    const char delimiter = ' ';
-    std::string_view token;
+  const char delimiter = ' ';
+  std::string_view token;
 
-    // Делим строку на токены по разделителю и перебираем эти токены:
-    while (NextToken(sv, delimiter, token)) {
-        // обрабатываем очередной token
-        // например, печатаем его на экране:
-        std::cout << token << "\n";
-    }
+  // Делим строку на токены по разделителю и перебираем эти токены:
+  while (NextToken(sv, delimiter, token)) {
+    // обрабатываем очередной token
+    // например, печатаем его на экране:
+    std::cout << token << "\n";
+  }
   return 0;
 }
